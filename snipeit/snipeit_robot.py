9528e8322d4daf944bc674833af3f622a479f8d4
Maciej Pijanowski <maciej.pijanowski@3mdeb.com>
2023-08-01 00:11:36 +0200

    snipeit_robot: add
    Signed-off-by: Maciej Pijanowski <maciej.pijanowski@3mdeb.com>

---
+++ snipeit/snipeit_robot.py
@@ -0,0 +1,21 @@
+import robot.api.logger
+import snipeit_api
+
+
+def snipeit_checkout(rte_ip):
+    asset_id = snipeit_api.get_asset_id_by_rte_ip(rte_ip)
+    success, data = snipeit_api.check_out_asset(asset_id)
+    if success:
+        robot.api.logger.info(f'Asset {asset_id} successfully checked out.')
+        return data
+    else:
+        raise AssertionError(f'Error checking out asset {asset_id}. Response data: {data}')
+
+def snipeit_checkin(rte_ip):
+    asset_id = snipeit_api.get_asset_id_by_rte_ip(rte_ip)
+    success, data = snipeit_api.check_in_asset(asset_id)
+    if success:
+        robot.api.logger.info(f'Asset {asset_id} successfully checked in.')
+        return data
+    else:
+        raise AssertionError(f'Error checking in asset {asset_id}. Response data: {data}')
